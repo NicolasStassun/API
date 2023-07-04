bb7e2cb921b440a3f04f84b1487a9bd5579d5089
NICOLAS STASSUN <nicolas_stassun@estudante.sesisenai.org.br>
2023-07-03 21:49:53 -0300

    0307

--- src/usuarios/models/usuario.js
+++ src/usuarios/models/usuario.js
@@ -22,30 +22,12 @@ Usuario.init({
         allowNull: false,
         unique: true
     },
-    cardPermissions: {
-        type: DataTypes.STRING,
-        get() {
-          return JSON.parse(this.getDataValue('cardPermissions'));
-        },
-        set(value) {
-          this.setDataValue('cardPermissions', JSON.stringify(value));
-        }
-      },
-      propertiesPermissions: {
-        type: DataTypes.STRING,
-        get() {
-          return JSON.parse(this.getDataValue('propertiesPermissions'));
-        },
-        set(value) {
-          this.setDataValue('propertiesPermissions', JSON.stringify(value));
-        }
-      }
-      
+    
 }, {
     sequelize: connection,
     modelName: 'users'
 });
 
-Usuario.sync({force: true}).then(() => {console.log('Usuario sincronizado')}).catch((e) => {console.log('Usuario nao sincronizado')})
+Usuario.sync().then(() => {console.log('Usuario sincronizado')}).catch((e) => {console.log('Usuario nao sincronizado')})
 
 module.exports = Usuario;
